2940ab2eb375d3a69ae19326a14b37f52e9c7883
mohamed-Goumih <85476868+mohamed-Goumih@users.noreply.github.com>
2023-10-18 17:19:36 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,5 +1,8 @@
+import java.util.Date;
 public class test{
     public static void main(String[] args) {
         System.out.println("Test java");
+          System.out.println(Date.now());
+        
     }
 }
